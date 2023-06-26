616b2db6b0d9ae98b33d36aef1382520660ccdf2
MD Nayem <nayemtp@gmail.com>
2023-06-26 20:23:16 +0600

    register page

--- oop-blog-project/admin/register-form.php
+++ oop-blog-project/admin/register-form.php
@@ -23,7 +23,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <title>Register</title>
 </head>
@@ -70,7 +71,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
 </body>
 
--- oop-blog-project/src/register.php
+++ oop-blog-project/src/register.php
@@ -26,5 +26,13 @@ class register
         $phone = $this->formet->validation($data['phone']);
         $email = $this->formet->validation($data['email']);
         $passwoed = $this->formet->validation($data['password']);
+        $_token = md5(rand());
+        $result_query = "SELECT * FROM tbl_user Where email = '$email'";
+        $check_email = $this->db->select($result_query);
+        if ($check_email > 0) {
+            $error = "This Email is alrady Exisit";
+            return $error;
+            header("location:register-form.php");
+        }
     }
 }
