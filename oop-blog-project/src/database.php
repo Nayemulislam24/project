eece6fae98cb004a72c2d0d03c96c1301c71ebec
MD Nayem <nayemtp@gmail.com>
2023-06-28 00:02:11 +0600

    verify mail

--- oop-blog-project/src/database.php
+++ oop-blog-project/src/database.php
@@ -10,7 +10,7 @@ class database
     private $db_host = "localhost";
     private $db_user = "root";
     private $db_pass = "";
-    private $db_name = "oop_project";
+    private $db_name = "project";
 
 
     public $connected;
--- oop-blog-project/src/register.php
+++ oop-blog-project/src/register.php
@@ -2,7 +2,10 @@
 
 namespace Appp;
 
+// require __DIR__ . '/vendor/autoload.php';
+
 require_once __DIR__ . '../../vendor/autoload.php';
+
 
 // use Appp\database;
 
@@ -17,6 +20,8 @@ class register
 {
     public $db;
     public $formet;
+    public $table;
+
     public function __construct()
     {
         $this->db = new database();
@@ -25,37 +30,43 @@ class register
 
     public function addUser($data)
     {
+        // $this->table->string('image')->nullable();
+
         function sendemailVarification($name, $email, $v_token)
         {
             $mail = new PHPMailer(true);
             try {
                 //Server settings
-                $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+                // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
                 $mail->isSMTP();                                            //Send using SMTP
-                $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+                $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
                 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-                $mail->Username   = 'user@example.com';                     //SMTP username
-                $mail->Password   = 'secret';                               //SMTP password
-                $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-                $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+                $mail->Username   = 'nayemtp@gmail.com';                     //SMTP username
+                $mail->Password   = '922129ma#python';                               //SMTP password
+                $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+                $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
                 //Recipients
-                $mail->setFrom('from@example.com', 'Mailer');
-                $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-                $mail->addAddress('ellen@example.com');               //Name is optional
-                $mail->addReplyTo('info@example.com', 'Information');
-                $mail->addCC('cc@example.com');
-                $mail->addBCC('bcc@example.com');
+                $mail->setFrom('nayemtp@gmail.com', $name);
+                // $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+                $mail->addAddress($email);               //Name is optional
+                // $mail->addReplyTo('info@example.com', 'Information');
+                // $mail->addCC('cc@example.com');
+                // $mail->addBCC('bcc@example.com');
 
                 //Attachments
-                $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-                $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+                // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+                // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
                 //Content
                 $mail->isHTML(true);                                  //Set email format to HTML
-                $mail->Subject = 'Here is the subject';
-                $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-                $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+                $mail->Subject = 'The verification code';
+                // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+                $email_tamplate = "<h2>You have register with web master</h2>
+                <h5>verify your email address to login pleace click the link blow</h5>
+                <a href='http://localhost/project/oop-blog-project/admin/verifi-email.php?token=$v_token'>click Here</a>";
+                $mail->Body = $email_tamplate;
+
 
                 $mail->send();
                 echo 'Message has been sent';
@@ -76,7 +87,7 @@ class register
         } else {
             $check_email_query = "SELECT * FROM tbl_user Where email = '$email'";
             $result_query = $this->db->select($check_email_query);
-            if ($result_query > 0) {
+            if ($result_query->num_rows > '0') {
                 $error = "This Email is alrady Exisit";
                 return $error;
                 header("location:register-form.php");
@@ -84,7 +95,7 @@ class register
                 $insert_query = "INSERT INTO tbl_user(username, email, phone, password, v_token) VALUES('$name','$email','$phone','$password','$v_token')";
                 $insert_row = $this->db->insert($insert_query);
                 if ($insert_row) {
-                    $sendemailVarification($name, $email, $v_token);
+                    sendemailVarification($name, $email, $v_token);
                     $success = "Registration Successfull. Pleace chck your email inbox for varifi email";
                     return $success;
                 } else {
