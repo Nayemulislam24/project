1283d6f3ba3bd9eaa9304f4e5201bf0d00133d3e
MD Nayem <nayemtp@gmail.com>
2023-09-03 02:21:06 +0600

    php error function

--- php-basic/php-mysqli/mysqli_error.php
+++ php-basic/php-mysqli/mysqli_error.php
@@ -1,10 +1,17 @@
-<h1>mysqli_fetch_assoc</h1>
+<h1>mysqli_error</h1>
 <?php
 // ==============Procedural style example==============
 
-$conn = mysqli_connect("localhost","root","","testdb_copy") or die("Database Not Connected");
-$sql = "select * from students";
+$conn = mysqli_connect("localhost", "root", "", "testdb_copy");
+
+if (!$conn) {
+    echo("Connection failed: " . mysqli_connect_error($conn));
+}
+$sql = "select * from student";
 $result = mysqli_query($conn, $sql) or die("Not Query Result");
+
+print_r(mysqli_error_list($conn));
+die();
 
 // ==============Object oriented style example===========
 
@@ -12,5 +19,13 @@ $result = mysqli_query($conn, $sql) or die("Not Query Result");
 // $sql = "select * from students";
 // $result = $conn->query($sql);
 
+
+
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo $row['roll'] . " " . $row['name'] . " " . $row['gender'] . " " . $row['age'];
+        echo "</br>";
+    }
+}
 
 ?>
