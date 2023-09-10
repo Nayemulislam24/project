8a222c57de417735e927d6a65204eeabebf2af5c
MD Nayem <nayemtp@gmail.com>
2023-09-10 20:10:56 +0600

    array prctic

---
+++ php-basic/php-array/array.php
@@ -0,0 +1,26 @@
+<?php 
+$coloers =array("red","blue","green","yellow","Blackl",50);
+
+// echo $coloers[0]."<br>";
+// echo $coloers[1]."<br>";
+// echo $coloers[2]."<br>";
+// echo $coloers[4] ."<br>";
+// echo $coloers[5] ."<br>";
+ 
+// echo "<pre>";
+// print_r($coloers);
+// echo "<pre>";
+ $n=6;
+for($i = 0; $i<$n;$i++){
+    echo $coloers[$i] . "<br>";
+}
+
+
+
+
+
+
+
+
+
+?>
