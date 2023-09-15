4ba3ec64f3468859aaa206499f0ffda1de11b333
MD Nayem <nayemtp@gmail.com>
2023-09-15 10:27:01 +0600

    array function intersect

---
+++ php-basic/php-array/array function/array intersect.php
@@ -0,0 +1,9 @@
+<?php
+$a1=array("a"=>"red","b"=>"green","c"=>"blue","d"=>"yellow");
+$a2=array("e"=>"red","f"=>"green","g"=>"blue");
+
+$result=array_intersect($a1,$a2);
+echo "<pre>";
+print_r($result);
+echo "</pre>";
+?>
