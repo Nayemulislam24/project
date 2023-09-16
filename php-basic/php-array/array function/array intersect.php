560da63650128945fee0f05c2d7a785e8cf98d09
MD Nayem <nayemtp@gmail.com>
2023-09-16 23:13:41 +0600

    Array function

--- php-basic/php-array/array function/array intersect.php
+++ php-basic/php-array/array function/array intersect.php
@@ -1,8 +1,22 @@
 <?php
+echo"<h1>array_intersect</h2>";
+
 $a1=array("a"=>"red","b"=>"green","c"=>"blue","d"=>"yellow");
 $a2=array("e"=>"red","f"=>"green","g"=>"blue");
 
 $result=array_intersect($a1,$a2);
+echo "<pre>";
+print_r($result);
+echo "</pre>";
+
+
+echo"<h1>array_intersect_assoc</h2>";
+
+
+$a1=array("a"=>"red","b"=>"green","c"=>"blue","d"=>"yellow");
+$a2=array("a"=>"red","b"=>"green","c"=>"blue","d"=>"white");
+
+$result=array_intersect_assoc($a1,$a2);
 echo "<pre>";
 print_r($result);
 echo "</pre>";
