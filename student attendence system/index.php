2412ea4a685a3c62e185d4542b69cf8adafce6b5
MD Nayem <nayemtp@gmail.com>
2023-09-27 00:14:05 +0600

    student  attendenc designe

--- student attendence system/index.php
+++ student attendence system/index.php
@@ -7,6 +7,8 @@
     <title>Student Attendence System</title>
     <link rel="stylesheet" href="inc/style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css">
 </head>
 
 <body>
@@ -14,33 +16,71 @@
         <div class="well text-center">
             <h2>Student Attendence System</h2>
         </div>
+        <div class="well text-center" style="font-size: 20px;">
+            <strong>Date:</strong><?php $cur_date = date('Y-m-d');
+                                    echo $cur_date; ?>
+        </div>
 
         <div class="panel panel-default">
             <div class="panel-heading">
                 <h2>
                     <a href="#" class="btn btn-success">Add Student</a>
+
                     <a href="#" class="btn btn-info pull-right">View All</a>
                 </h2>
             </div>
             <div class="panel-body">
                 <form action="" method="post">
-                    <table class="table table-striped">
-                        <tr>
-                            <th width="25%">Sereal</th>
-                            <th width="25%">Student Name</th>
-                            <th width="25%">Roll</th>
-                            <th width="25%">Attendence</th>
-                        </tr>
-                        <tr>
-                            <td>01</td>
-                            <td>Nayemul Islam</td>
-                            <td>01</td>
-                            <td>
-                            <input class="form-check-input" type="radio" name="Attend" value="Attend" id="">P
-                            <input class="form-check-input" type="radio" name="Absent" value="Attend" id="">A
-                            </td>
-                        </tr>
+                    <table class="table table-striped" id="dataTable">
+                        <thead>
+                            <tr>
+                                <th width="25%">Sereal</th>
+                                <th width="25%">Student Name</th>
+                                <th width="25%">Roll</th>
+                                <th width="25%">Attendence</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>01</td>
+                                <td>Nayemul Islam</td>
+                                <td>01</td>
+                                <td>
+                                    <input class="form-check-input" type="radio" name="Attend" value="Attend" id="">P
+                                    <input class="form-check-input" type="radio" name="Absent" value="Attend" id="">A
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>02</td>
+                                <td>Nayemul Islam</td>
+                                <td>02</td>
+                                <td>
+                                    <input class="form-check-input" type="radio" name="Attend" value="Attend" id="">P
+                                    <input class="form-check-input" type="radio" name="Absent" value="Attend" id="">A
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>03</td>
+                                <td>Nayemul Islam</td>
+                                <td>03</td>
+                                <td>
+                                    <input class="form-check-input" type="radio" name="Attend" value="Attend" id="">P
+                                    <input class="form-check-input" type="radio" name="Absent" value="Attend" id="">A
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>04</td>
+                                <td>Nayemul Islam</td>
+                                <td>04</td>
+                                <td>
+                                    <input class="form-check-input" type="radio" name="Attend" value="Attend" id="">P
+                                    <input class="form-check-input" type="radio" name="Absent" value="Attend" id="">A
+                                </td>
+                            </tr>
+                        </tbody>
+
                     </table>
+                    <button class="btn btn-primary text-center" type="submit">Submit</button>
                 </form>
             </div>
         </div>
@@ -50,10 +90,25 @@
     </section>
 
 
+
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <!-- datable library -->
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
     <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#dataTable').DataTable({
+                // dom: 'Bfrtip',
+                // buttons: [
+                //     'copyHtml5',
+                //     'excelHtml5'
+
+                // ]
+            });
+        });
+    </script>
 </body>
 
 </html>
