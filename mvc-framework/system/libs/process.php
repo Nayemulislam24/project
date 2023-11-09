9ec4197549f63c50ebb5bfb196f118e65e991633
MD Nayem <nayemtp@gmail.com>
2023-11-09 19:31:44 +0600

    oop practice

--- mvc-framework/system/libs/process.php
+++ mvc-framework/system/libs/process.php
@@ -7,6 +7,7 @@ class process
     public function __construct()
     {
         echo " load class test parent  controller.<br>";
+        echo " load class test parent  controller.<br>";
     }
     public function view($file_name){
         include'app/views/'.$file_name.'.php';
